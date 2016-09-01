ac7bf01560124f2c2b722c9284517e16b2f893a9
Chris Freeman <cfreeman.code@gmail.com>
2016-09-01 15:57:21 -0400

    inital config setup

---
+++ karma.conf.js
@@ -0,0 +1,36 @@
+var webpackConfig = require('./webpack.config');
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai'],
+    files: [
+      'test/**/*.spec.ts'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+      'test/**/*spec.ts': ['webpack']
+    },
+    webpack: {
+      module: webpackConfig.module,
+      resolve: webpackConfig.resolve
+    },
+    reporters: ['mocha'],
+    mochaReporter: {
+      showDiff: true,
+    },
+    client: {
+      mocha: {
+        reporter: 'html'
+      }
+    },
+    port: 3001,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
